721f11a076420aad6839689d663cfa5d986316d5
yabutaka <tacky0630@gmail.com>
2019-05-09 21:24:11 -0700

    Initial iOS app

---
+++ Anime-iOS/Anime-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Anime-iOS
+//
+//  Created by 薮田隆司 on 09/05/2019.
+//  Copyright © 2019 Takashi.Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
