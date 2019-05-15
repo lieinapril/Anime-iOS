923db391266704eb4005aca9ff986163e3ab1f35
Weiran Xiong <weiranxiong@gmail.com>
2019-05-14 22:00:40 -0700

    Create anime struct

---
+++ Anime-iOS/Anime-iOS/Models/Anime.swift
@@ -0,0 +1,18 @@
+//
+//  Anime.swift
+//  Anime-iOS
+//
+//  Created by Weiran Xiong on 5/14/19.
+//  Copyright © 2019 Takashi.Co. All rights reserved.
+//
+
+import Foundation
+
+struct Anime {
+    
+    let title: String
+    let description: String
+    let posterImgLink: URL
+    let viewingUrl: URL
+    
+}
